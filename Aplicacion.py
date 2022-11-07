2b80eb2d2e7374b7864ba2ca69ce82c6f7478fa5
Thalia Vargas <20A3050036@untels.edu.pe>
2022-11-07 12:52:53 -0500

    Parte 4

--- Aplicacion.py
+++ Aplicacion.py
@@ -2,18 +2,75 @@ from os import system
 import time
 import sqlite3
 
+conexion = sqlite3.connect("Parillo_Vargas_Wong_almacen.db")
 
 def crear():
-    exit()
+    codigo = str(input("INGRESA EL CODIGO: "))
+    nombre = str(input("INGRESE SU NOMBRE: "))
+    precio = int(input("INGRESE SU PRECIO: "))
+    if(len(codigo) > 0 and len(nombre) > 0 ):
+        consulta = """ INSERT INTO
+                   producto(codigo,nombre,precio)
+                   VALUES(?,?,?)
+               """
+        parametros =(codigo,nombre,precio)
+        cursor = conexion.cursor()
+        cursor.execute(consulta,parametros)
+        conexion.commit()
+        conexion.close()
 
 def listar():
-    exit()
+    consulta = """ SELECT *
+                   FROM producto
+               """
+    cursor = conexion.cursor()
+    productos =  cursor.execute(consulta)
+ 
+
+    #Acá libros es una lista ...
+   
+    
+    for data in productos:
+        print(""" 
+        ID :        {}
+        codigo :    {}
+        nombre :     {}
+        precio :     {}
+        """.format(data[0],data[1],data[2],data[3]))
+    conexion.close()
     
 def modificar():
-    exit()
+    id = int(input("INGRESA EL ID: "))
+    if(id != 0):
+      codigo = str(input("INGRESA EL CODIGO: "))
+      nombre = str(input("INGRESE SU NOMBRE: "))
+      precio = int(input("INGRESE SU PRECIO: "))
+      consulta = """ UPDATE producto
+                   SET
+                      codigo = ?,
+                      nombre = ?,
+                      precio = ?
+                   WHERE
+                        idproducto = ?
+               """
+    parametros = (codigo,nombre,precio,id)
+    cursor = conexion.cursor()
+    cursor.execute(consulta,parametros)
+    conexion.commit()
+    conexion.close()
 
 def eliminar():
-    exit()
+    id = int(input("INGRESA EL ID: "))
+    if(id != 0):
+        consulta = """ DELETE FROM producto
+                   WHERE
+                        idproducto = ?
+               """
+    parametro=(id,)
+    cursor = conexion.cursor()
+    cursor.execute(consulta,parametro)
+    conexion.commit()
+    conexion.close()
     
 def salir():
     exit()
@@ -36,13 +93,13 @@ while True:
             crear()
             
         elif (opcion == 2):
-            listar()
+            eliminar()
             
         elif (opcion == 3):
             modificar()
             
         elif (opcion == 4):
-            eliminar()
+            listar()
 
         elif (opcion == 5):
             salir()
